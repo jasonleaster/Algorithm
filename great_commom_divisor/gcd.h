5c9c444f948e26ce835da281ca22c6d83b064e4a
Jason Leaster <jasonleaster@gmail.com>
2014-09-18 13:18:22 +0800

    GCD

---
+++ great_commom_divisor/gcd.h
@@ -0,0 +1,5 @@
+#ifndef _GCD_H
+#define _GCD_H
+
+	unsigned int gcd(unsigned int x,unsigned int y);
+#endif
---
+++ great_commom_divisor/gcd_test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "gcd.h"
+
+int main()
+{
+	printf("gcd(1989,1590) :%d\n",gcd(1989,1590));
+	return 0;
+}
