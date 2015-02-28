a006ca4db8ba1f51d6d735d1ace888a35bd56f7f
Jason Leaster <jasonleaster@gmail.com>
2015-02-28 20:14:22 +0800

    新文件:       String_Matching/Automaton/fam.py

---
+++ String_Matching/Automaton/fam.py
@@ -0,0 +1,20 @@
+def finite_automaton_matcher(T, delta, m) :
+    n = len(T)
+    q = 0
+
+    for i in range(0, n)
+        q = delta(q, T[i])
+        if q == m :
+            print "Pattern occurs with shift ", s
+
+def compute_transition_function(P, sigma) :
+    m = len(P)
+    for q in range(0, m)
+        for a in sigma :
+            k = min(m+1, q+2)
+                while .... :
+                    k -= 1
+            delta(q, a) = k
+
+    return delta
+
---
+++ String_Matching/KMP/kmp.py
@@ -0,0 +1,50 @@
+"""
+Programmer  :   EOF
+Code date   :   2015.02.28
+Code file   :   kmp.py
+e-mail      :   jasonleaster@gmail.com
+
+Code description    :
+    Here is a implementation of KMP which is a useful
+algorithm for string matching.
+
+"""
+
+def kmp_matcher(T, P) :
+    n = len(T)
+    m = len(P)
+
+    pi = compute_prefix_function(P)
+    q = -1 # number of characters matched
+    for i in range(0, n) : # scan the text from left to right
+        while q > 0 and P[q+1] != T[i] :
+            q = pi[q]       # next character doesn't match
+
+        if P[q+1] == T[i] :
+            q += 1          # next character matches
+        if (q+1) == m :         # Is all of P matched ?
+            print "Pattern occurs with shift ", i-m
+            q = pi[q]       # look for the next match
+
+
+def compute_prefix_function(P) :
+    m = len(P)
+    pi = [-1 for i in range(0, m)]
+
+    k = -1 # Attention !
+    for q in range(1, m) :
+        while k > 0 and P[k+1] != P[q] :
+            k = pi[k]
+        if P[k+1] == P[q] :
+            k += 1
+
+        pi[q] = k
+
+    return pi
+
+#-------------for testing----------------------
+#string_1 = "hello goodbye and hello"
+#string_2 = "hello"
+string_1 = "abcabaabcabac"
+string_2 = "abaa"
+kmp_matcher(string_1, string_2)
